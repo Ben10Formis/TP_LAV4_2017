30d1262613e24277584ea7360288120a7f453762
Ben10Formis <ibarraromyna@gmail.com>
2018-08-30 00:47:57 -0300

    Juego Agilidad Aritmética (error bucle)

--- src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
+++ src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
@@ -9,25 +9,28 @@ import {TimerObservable} from "rxjs/observable/TimerObservable";
   styleUrls: ['./agilidad-aritmetica.component.css']
 })
 export class AgilidadAritmeticaComponent implements OnInit {
-   @Output() 
+  @Output() 
   enviarJuego :EventEmitter<any>= new EventEmitter<any>();
   nuevoJuego : JuegoAgilidad;
   ocultarVerificar: boolean;
   Tiempo: number;
   repetidor:any;
   private subscription: Subscription;
-  ngOnInit() {
-  }
-   constructor() {
+  Mensajes:string;
+ // aux : boolean;
+
+  ngOnInit() { }
+
+  constructor() {
      this.ocultarVerificar=true;
-     this.Tiempo=5; 
-    this.nuevoJuego = new JuegoAgilidad();
-    console.info("Inicio agilidad");  
+     this.Tiempo=10; 
+    this.nuevoJuego = new JuegoAgilidad("Agilidad Aritmetica",false);
+    console.info("Inicio agilidad: estoy en el constructor"); 
+    console.info("1111111111"); 
   }
-  NuevoJuego() {
+/*  NuevoJuego() {
     this.ocultarVerificar=false;
-   this.repetidor = setInterval(()=>{ 
-      
+    this.repetidor = setInterval(()=>{ 
       this.Tiempo--;
       console.log("llego", this.Tiempo);
       if(this.Tiempo==0 ) {
@@ -36,16 +39,73 @@ export class AgilidadAritmeticaComponent implements OnInit {
         this.ocultarVerificar=true;
         this.Tiempo=5;
       }
-      }, 900);
-
+    }, 900);
+  }*/
+  NuevoJuego() {
+    this.ocultarVerificar=false; //como inicia el juego necesito que el usuario cheque si gano o no.
+    console.info("2222222222");
+    this.nuevoJuego.generarOperacion();
+    this.repetidor = setInterval(()=>{ 
+      this.Tiempo--;
+      console.log("llego", this.Tiempo);
+      if(this.Tiempo==0 ) {
+        clearInterval(this.repetidor);
+        this.verificar();
+        this.ocultarVerificar=true;
+        this.Tiempo=15;
+      }
+    }, 900);
   }
-  verificar()
-  {
+/*  verificar()  {
     this.ocultarVerificar=false;
     clearInterval(this.repetidor);
-   
+  }*/
+  verificar()  {
+    console.info("333333333");
+    this.ocultarVerificar=false;
+    clearInterval(this.repetidor);
+   // if(this.nuevoJuego.gano){
+  if(this.nuevoJuego.verificar()){
+    // this.aux = true;
+    this.MostarMensaje("Ganaste!!!",true);
+  }
+  else{
+    //this.aux = false;
+    this.MostarMensaje("Casi pero nop. Perdiste!",false); 
+  }
+  this.ocultarVerificar = true;
+  this.Tiempo = 7;
+  this.enviarJuego.emit(this.nuevoJuego);
+  //this.limpiarIntervalo();
+  console.info("dentro del 3 el 55555555555555");
+  this.nuevoJuego = new JuegoAgilidad("Agilidad Aritmetica",false);//para borrar el anterior
+  } 
+  limpiarIntervalo() {
+    console.info("limpiarIntervalo");
+    console.info("55555555555555");
+    clearInterval(this.repetidor);
+    this.ocultarVerificar=true;
+    this.Tiempo=10;
+    console.info("new JuegoAgilidad(Agilidad Aritmetica");
+    this.nuevoJuego = new JuegoAgilidad("Agilidad Aritmetica",false);//para borrar el anterior
+  }
+  MostarMensaje(mensaje:string="fc MostrarMensaje()",gano:boolean=false) {
+    console.info("44444444444444444");
+    this.Mensajes = mensaje;    
+    var x = document.getElementById("snackbar");
+    if(gano)
+    {
+      x.className = "show Ganador";
+    }else{
+      x.className = "show Perdedor";
+    }
 
-   
-  }  
-
+    var modelo = this;
+    setTimeout(function(){ 
+      x.className = x.className.replace("show", "");
+      modelo.ocultarVerificar=false;
+    }, 3000);
+    console.info("objeto",x);
+  
+   }
 }
